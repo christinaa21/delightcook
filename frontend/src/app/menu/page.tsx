d0e65b35e90c73561130ec826845383433c40308
christinaa21 <16521224@mahasiswa.itb.ac.id>
2023-12-07 16:58:41 +0700

    Feat: Navbar, font styling

--- frontend/src/app/menu/page.tsx
+++ frontend/src/app/menu/page.tsx
@@ -1,10 +1,14 @@
-import { ChakraProvider, Heading } from "@chakra-ui/react";
+import { ChakraProvider, Heading, Image } from "@chakra-ui/react";
 import Navbar from "@/components/Navbar";
 
 export default function Menu(){
     return(
         <ChakraProvider>
-            <Navbar />
+            <Navbar status={true} />
+            <Image
+                src="./landing.png"
+                objectFit={'cover'}
+                height={"440px"}/>
             <Heading>
                 Menu
             </Heading>
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,14 +6,19 @@ import {
     Link,
     Image,
     Button,
+    HStack,
+    Heading
   } from "@chakra-ui/react";
+import '@fontsource/cinzel'
 
-export default function Navbar(){
+export default function Navbar(props: {status: boolean}){
+    const isLogin = (props.status == true);
+    const isNotLogin = (props.status == false);
     return(
         <Box
-          bg="#FDB235"
-          color="#02033B"
-          p={4}
+          bg="#3FC3FE"
+          color="#FFFFFF"
+          p={1}
           fontSize="lg"
           fontWeight="bold"
           display={{ md: "flex" }}
@@ -24,36 +29,101 @@ export default function Navbar(){
           marginX="auto"
           position="relative"
         >
-            <Image
-                src="./logo.png"
-                alt="Logo"
-                boxSize={32}
-                h={12}
-                objectFit={"contain"}
-            />
-            <Flex display={{ base: "none", md: "flex" }}>
-                <Box
-                    mx={10}
-                    position="relative"
-                    fontWeight="bold"
-                    padding= '10px'
-                    style={{ whiteSpace: 'nowrap' }}
-                    _hover={{
-                    color: 'white',
-                    bgColor: '#02033B',
-                    rounded: 'md',
-                    }}
-                >
-                    <Button
-                        fontWeight="bold" 
-                        _hover={{
-                            color: 'white',
-                        }}
+            <HStack>
+                <Image
+                    src="./logo.png"
+                    alt="Logo"
+                    ml={8}
+                    mr={2}
+                    h={12}
+                    objectFit={"contain"}
+                />
+                <Heading
+                fontFamily={'cinzel'}>
+                    Delight Cook
+                </Heading>
+            </HStack>
+            <Flex>
+                { isNotLogin && 
+                    <HStack
+                        mx={14}
+                        position="relative"
+                        fontWeight="bold"
+                        padding= '10px'
+                        style={{ whiteSpace: 'nowrap' }}
                     >
-                        <a href="./menu">Menu</a>
-                    </Button>
-                    
-                </Box>
+                        <Button
+                            fontWeight="bold" 
+                            color={"white"}
+                            bg={"#3FC3FE"}
+                            _hover={{
+                                bg: '#0CB3FE',
+                            }}
+                            variant={"ghost"}
+                            mx={1}
+                        >
+                            <a href="./menu">Menu</a>
+                        </Button>
+                        <Button
+                            fontWeight="bold" 
+                            color={"white"}
+                            bg={"#3FC3FE"}
+                            _hover={{
+                                bg: '#0CB3FE',
+                            }}
+                            variant={"ghost"}
+                            mx={1}
+                        >
+                            <a href="./login">Login</a>
+                        </Button>
+                    </HStack>
+                }
+                { isLogin && 
+                    <HStack
+                        mx={14}
+                        position="relative"
+                        fontWeight="bold"
+                        padding= '10px'
+                        style={{ whiteSpace: 'nowrap' }}
+                    >
+                        <Button
+                            fontWeight="bold" 
+                            color={"white"}
+                            bg={"#3FC3FE"}
+                            _hover={{
+                                bg: '#0CB3FE',
+                            }}
+                            variant={"ghost"}
+                            mx={1}
+                        >
+                            <a href="./menu">Menu</a>
+                        </Button>
+                        <Button
+                            fontWeight="bold" 
+                            color={"white"}
+                            bg={"#3FC3FE"}
+                            _hover={{
+                                bg: '#0CB3FE',
+                            }}
+                            variant={"ghost"}
+                            mx={1}
+                        >
+                            <a href="./history">History</a>
+                        </Button>
+                        <Button
+                            fontWeight="bold" 
+                            color={"white"}
+                            bg={"#3FC3FE"}
+                            _hover={{
+                                bg: '#0CB3FE',
+                            }}
+                            variant={"ghost"}
+                            mx={1}
+                        >
+                            <a href="./logout">Logout</a>
+                        </Button>
+                    </HStack>
+                }
             </Flex>
         </Box>
     )
